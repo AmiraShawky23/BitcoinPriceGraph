3e3a4d5c138679e15db40fff14160fa53d1f4cb3
Amira <amirashawkym23@gmail.com>
2021-09-14 20:08:23 +0200

    Custom modules for form data
    Removing create react app initial styles, initial home page content, and logo -initializing useFetch for data calling and useForm for forms' state management

---
+++ src/useFetch.js
@@ -0,0 +1,7 @@
+import { useEffect, useState } from "react";
+
+export const useFetch = (url) => {
+    // https://api.coindesk.com/v1/bpi/historical/close.json?start=2021-08-01&end=2021-08-10&index=[USD]
+    // let data = bpi list
+    return data;
+}
